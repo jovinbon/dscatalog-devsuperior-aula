a06b4f7bdc77fefc7fdc41e15cf5638c081e2484
Cristiano Fontes <jovinbon@hotmail.com>
2021-11-15 19:53:41 -0300

    DTO

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,4 +1,5 @@
 INSERT INTO tb_category (name) VALUES ('Books');
 INSERT INTO tb_category (name) VALUES ('Electronics');
 INSERT INTO tb_category (name) VALUES ('Computers');
+INSERT INTO tb_category (name) VALUES ('Laptops');
 INSERT INTO tb_category (name) VALUES ('Smartphones');
